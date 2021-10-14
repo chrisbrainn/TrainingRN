8f54b341c3dc4970e33239d9840ccabe7c8df829
Christians Salim <christians.salim@binus.edu>
2021-10-14 12:10:28 +0700

    Hello World

---
+++ src/screens/Counter.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, SafeAreaView } from 'react-native';
+
+const CounterScreen = () => {
+    return (
+        <SafeAreaView>
+            <Text>Hello World</Text>
+        </SafeAreaView>
+    )
+}
+
+export default CounterScreen;
